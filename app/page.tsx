9309fdc6abf8fb588b516a4d4366c1629cf2a7c8
Shashikiran200212 <134192630+Shashikiran200212@users.noreply.github.com>
2024-08-11 21:52:20 +0530

    loader update

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,45 @@
+"use client"
+
+import { useState, useEffect } from "react";
 import Hero from "@/components/Hero";
 import Showcase from "@/components/Showcase";
 
+const BounceLoader = () => {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black">
+      <div className="flex flex-col items-center justify-center space-y-4">
+        <img src="icon.svg" alt="Loading icon" className="w-[500px] h-[500px]" />
+        <div className="flex items-center space-x-2">
+          <div className="h-5 w-5 animate-bounce rounded-full bg-white [animation-delay:-0.3s]"></div>
+          <div className="h-5 w-5 animate-bounce rounded-full bg-white [animation-delay:-0.13s]"></div>
+          <div className="h-5 w-5 animate-bounce rounded-full bg-white"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a loading delay
+    const timer = setTimeout(() => setLoading(false), 2000); // 2 seconds
+
+    // Cleanup function
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <main style={{ userSelect: 'none', }} className="bg-white">
-      <Hero/>
-      <Showcase/>
+    <main style={{ userSelect: 'none' }} className="bg-white">
+      {loading ? <BounceLoader /> : (
+        <>
+          <Hero />
+          <Showcase />
+        </>
+      )}
     </main>
   );
 }
